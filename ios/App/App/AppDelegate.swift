0a7baa2f99f70894fed116a2c8e4ee91ae861ef2
TradeLine 24/7 <jrmendozaceo@apexbusiness-systems.com>
2026-01-25 21:04:02 -0700

    fix(ios): mark first application parameter as unused

--- ios/App/App/AppDelegate.swift
+++ ios/App/App/AppDelegate.swift
@@ -6,7 +6,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions _: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+    func application(_: UIApplication, didFinishLaunchingWithOptions _: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         return true
     }
